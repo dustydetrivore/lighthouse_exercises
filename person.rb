16ea646421c4ae378bc5282e50528fd8ad0f329e
dustydetrivore <chris.m.matthew@gmail.com>
2015-02-18 20:51:11 -0800

    w3d2_work

--- person.rb
+++ person.rb
@@ -1,10 +1,11 @@
 class Person
-  attr_reader :first_name, :last_name, :gender
+  attr_reader :first_name, :last_name, :gender, :education
 
   def initialize(first_name, last_name, gender)
     @first_name = first_name
     @last_name = last_name
     @gender = gender
+    @education = education
   end
 
   def fullname
@@ -12,13 +13,39 @@ class Person
   end
 
   def doctor
-    "Dr. #{self.fullname}"
+    "Dr. #{self.fullname} [#{gender}]"
   end
 
   def lawyer
-    "#{fullname}, Esq."
+    "#{fullname}, Esq. [#{gender}]"
   end
 end
 
-person = Person.new("Chris", "Matthew", "Male")
-puts person.lawyer
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts "Are you a male or a female?"
+gender = gets.chomp
+
+puts "Are you a doctor or a lawyer?"
+education = gets.chomp
+
+person = Person.new(first_name, last_name, gender)
+
+if education == "doctor"
+  puts person.doctor
+elsif education == "lawyer"
+  puts person.lawyer
+else
+  puts person.fullname
+end
+
+
+
+
+# puts person.lawyer
+# person = Person.new("Chris", "Matthew", "Male")
+# puts person.lawyer
